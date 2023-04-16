914fa9829ac57aaa953fe8cc9ca721f0fe101c3a
SultanIndian007 <amantiwari201608@gmail.com>
2023-04-16 15:56:19 +0530

    failure-success-page-improved

--- medicall/src/pages/Success.js
+++ medicall/src/pages/Success.js
@@ -2,21 +2,13 @@ import React from "react";
 
 const Success = () => {
   return (
-    <div className="min-h-[650px] flex items-center">
-      <div>
-        <div className="max-w-[600px] rounded-lg p-5 border border-black mx-auto flex flex-col">
-          <div className="text-2xl font-bold">Thanks for shopping with us!</div>
-          <div className="text-lg font-bold mt-2">
-            Your order has been placed successfully.
-          </div>
-          <div className="text-base mt-5">
-            For any product related query, drop an email to
-          </div>
-          <div className="underline">medicall@gmail.com</div>
-        </div>
+    <div id="success">
+      <div className="home">
+        <h1>Payment Successful!!!</h1>
+        <br></br>
+        <h3>Thank you for choosing MEDICALL!</h3>
       </div>
     </div>
   );
 };
-
 export default Success;
