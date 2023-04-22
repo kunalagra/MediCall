ff5ec0f6c8fd01cc1f425168e700d7eebe122ad4
Ganesh Utla <ganeshutla888@gmail.com>
2023-04-22 15:12:48 +0530

    Updated Prescription

--- medicall/src/pages/MeetPage.js
+++ medicall/src/pages/MeetPage.js
@@ -38,8 +38,10 @@ const MeetPage = () => {
   const email = localStorage.getItem("email");
   const phone = localStorage.getItem("phone");
   const [prescription, setPrescription] = useState([]);
-  const [newPrescription, setNewPrescription] = useState([]);
+  const [newPrescription, setNewPrescription] = useState({name: "", dosage: "", duration: "", durationUnit: "day(s)", dosageTime: "Before Food"});
   const [copyAlert, setCopyAlert] = useState(false);
+  const [isInvDosage, setInvDosage] = useState(false);
+  const [isInvDuration, setInvDuration] = useState(false);
 
   useDocTitle("Meet");
 
@@ -139,6 +141,7 @@ const MeetPage = () => {
           display: "flex",
           justifyContent: "center",
         }}
+        className="renderButtons"
       >
         <button
           type="text"
@@ -229,8 +232,9 @@ const MeetPage = () => {
   }
 
   const addPrescriptionItem = () => {
-    setPrescription([...prescription, newPrescription]);
-    setNewPrescription("");
+    const newP = `${newPrescription.name} | ${newPrescription.dosage} (${newPrescription.dosageTime}) | ${newPrescription.duration} ${newPrescription.durationUnit}`;
+    setPrescription([...prescription, newP]);
+    setNewPrescription({name: "", dosage: "", duration: "", durationUnit: "day(s)", dosageTime: "Before Food"});
   }
 
   const handleFormSubmit = () => {
@@ -302,7 +306,7 @@ const MeetPage = () => {
     y += 50
 
     // const pre = ["Metodyl", "Cough Syrup", "Lokinozol", "Hexagoanl", "Qaudagozone", "Lipaipo"];
-    const l1 = [[1,"Consultation Fee", 200], [2,"Doctor Fee", 150], ...prescription.map((item, index) => [index+3, item, 0])];
+    const l1 = [...prescription.map((item, index) => [index+3, item, 0])];
     const totalCost = 350;
 
     pdf.autoTable({
@@ -379,22 +383,21 @@ const MeetPage = () => {
     
     pdf.text("MEDICALL", (pdf.internal.pageSize.width - 40 - (pdf.getStringUnitWidth("MEDICALL") * pdf.internal.getFontSize())), y);
     y += 30;
-    
     pdf.setFont("Helvetica", "normal");
     pdf.setFontSize(15);
     pdf.setTextColor(0, 0, 0);
-
+    
     pdf.text("Mumbai, India", (pdf.internal.pageSize.width - 40 - (pdf.getStringUnitWidth("Mumbai, India") * pdf.internal.getFontSize())), y);
     y += 20;
     pdf.text("+91 12345 67890", (pdf.internal.pageSize.width - 40 - (pdf.getStringUnitWidth("+91 12345 67890") * pdf.internal.getFontSize())), y);
     y += 20;
     pdf.text("medicall@gmail.com", (pdf.internal.pageSize.width - 40 - (pdf.getStringUnitWidth("medicall@gmail.com") * pdf.internal.getFontSize())), y);
     y += 25;
-
+    
     pdf.setFillColor(74, 76, 178);
     pdf.setDrawColor(74, 76, 178);
     pdf.rect(40, y, 515, 5, "FD");
-
+    
     y += 50;
     pdf.setFontSize(15);
     const name = localStorage.getItem("username")? localStorage.getItem("username") : "Mr. ABC DEF";
@@ -402,13 +405,13 @@ const MeetPage = () => {
     const gender = localStorage.getItem("gender")? localStorage.getItem("gender") : "NA";
     const d = new Date();
     const date = (d.getDate() < 10? '0' + d.getDate() : d.getDate()) + '/' + (d.getMonth() < 10? '0' + d.getMonth() : d.getMonth()) + '/' + d.getFullYear();
-    const selectedDoc = searchparams.get("selectedDoc");
+    const selectedDoc = searchparams.get("selectedDoc")? searchparams.get("selectedDoc") : "Doctor_Name";
     
     pdf.setFont("Times New Roman", "bold");
     pdf.text("Name: ", 40, y);
     pdf.setFont("Times New Roman", "normal");
     pdf.text(name, 45 + pdf.getStringUnitWidth("Name: ") * pdf.internal.getFontSize(), y);
-
+    
     pdf.setFont("Times New Roman", "bold");
     pdf.text("Age: ", 275, y);
     pdf.setFont("Times New Roman", "normal");
@@ -418,27 +421,31 @@ const MeetPage = () => {
     pdf.text("Gender: ", pdf.internal.pageSize.width - 45 - pdf.getStringUnitWidth(`Gender: ${gender}`) * pdf.internal.getFontSize(), y);
     pdf.setFont("Times New Roman", "normal");
     pdf.text(gender, pdf.internal.pageSize.width - 40 - pdf.getStringUnitWidth(gender) * pdf.internal.getFontSize(), y);
-
+    
     y += 30
     pdf.setFont("Times New Roman", "bold");
     pdf.text("Consulted By: ", 40, y);
     pdf.setFont("Times New Roman", "normal");
     pdf.text(selectedDoc, 45 + pdf.getStringUnitWidth("Consulted By: ") * pdf.internal.getFontSize(), y);
-
+    
     pdf.setFont("Times New Roman", "bold");
     pdf.text("Date: ", pdf.internal.pageSize.width - 45 - pdf.getStringUnitWidth(`Date: ${date}`) * pdf.internal.getFontSize(), y);
     pdf.setFont("Times New Roman", "normal");
     pdf.text(date, pdf.internal.pageSize.width - 40 - pdf.getStringUnitWidth(date) * pdf.internal.getFontSize(), y);
-
-    y += 50
-
-    // const pre = ["Metodyl", "Cough Syrup", "Lokinozol", "Hexagoanl", "Qaudagozone", "Lipaipo"];
-    const l1 = [[1,"Consultation Fee", 200], [2,"Doctor Fee", 150], ...prescription.map((item, index) => [index+3, item, 0])];
+    
+    y += 50;
+    
     const totalCost = 350;
-
+    
+    pdf.setFont("Times New Roman", "bold");
+    pdf.setDrawColor(0, 0, 0);
+    pdf.text("Prescription", pdf.internal.pageSize.width/2 - (pdf.getStringUnitWidth("Prescription")/2) * (pdf.internal.getFontSize()/2), y);
+    pdf.line(pdf.internal.pageSize.width/2 - (pdf.getStringUnitWidth(`Prescription`)/2) * (pdf.internal.getFontSize()/2), y+5, pdf.internal.pageSize.width/2 - (pdf.getStringUnitWidth(`Prescription`)/2) * (pdf.internal.getFontSize()/2) + 80, y+5);
+    y += 15;
+    
     pdf.autoTable({
-      head: [["SrNo", "Item", "Cost (in Rs.)"]],
-      body: l1,
+      head: [["SrNo", "Medicine", "Dosage (Morning-Afternoon-Night)", "Duration"]],
+      body: [...prescription.map((item, index) => [index+1, ...item.split(" | ")])],
       startY: y,
       headStyles: {
         valign: "middle",
@@ -452,7 +459,30 @@ const MeetPage = () => {
       }
     });
 
-    y += (30 * l1.length) + 10;
+    y += (30 * prescription.length) + 30;
+
+    pdf.setFont("Times New Roman", "bold");
+    pdf.text("Fee Details", pdf.internal.pageSize.width/2 - (pdf.getStringUnitWidth("Fee Details")/2) * (pdf.internal.getFontSize()/2), y);
+    pdf.line(pdf.internal.pageSize.width/2 - (pdf.getStringUnitWidth(`Fee Details`)/2) * (pdf.internal.getFontSize()/2), y+5, pdf.internal.pageSize.width/2 - (pdf.getStringUnitWidth(`Fee Details`)/2) * (pdf.internal.getFontSize()/2) + 72, y+5);
+    y += 15;
+
+    pdf.autoTable({
+      head: [["SrNo", "Name", "Cost (in Rs.)"]],
+      body: [[1,"Consultation Fee", 150], [2,"Doctor Fee", 200]],
+      startY: y,
+      headStyles: {
+        valign: "middle",
+        halign: "center",
+        fontSize: 13
+      },
+      bodyStyles: {
+        valign: "middle",
+        halign: "center",
+        fontSize: 12
+      }
+    });
+
+    y += (30 * 2) + 45;
 
     pdf.setFont("Times New Roman", "bold");
     pdf.text("Total Cost: ", pdf.internal.pageSize.width - 45 - pdf.getStringUnitWidth(`Total Cost: Rs. ${totalCost}`) * pdf.internal.getFontSize(), y);
@@ -464,13 +494,13 @@ const MeetPage = () => {
     pdf.setFontSize(13);
     pdf.text("NOTE: ", 40, y);
     pdf.setFont("Times New Roman", "italic");
-    pdf.text('The cost for the medicines in the prescription are given 0 as the patient have to buy these', 45 + pdf.getStringUnitWidth("NOTE: ") * pdf.internal.getFontSize(), y);
+    pdf.text('The cost for the medicines in the prescription is not given as the patient have to buy these', 45 + pdf.getStringUnitWidth("NOTE: ") * pdf.internal.getFontSize(), y);
     y += 18;
     pdf.text("medicines from the medical store. You can purchase these medicines from the given link: ", 40, y);
     y += 18;
-    pdf.text("https://medicall.com/buy-medicines.", 40, y);
+    pdf.text("https://gfg-sfi.onrender.com/buy-medicines.", 40, y);
 
-    y += 100;
+    y += 60;
     pdf.setFont("Times New Roman", "bold");
     pdf.setFontSize(15);
     pdf.text("Signature: ", 40, y);
@@ -480,8 +510,7 @@ const MeetPage = () => {
     pdf.setFont("Helvetica", "normal");
     pdf.setFontSize(12);
     pdf.setTextColor(150);
-    pdf.text("2023 @MediCall | All Rights Reserved", pdf.internal.pageSize.width - 40 - pdf.getStringUnitWidth("2023 @MediCall | All Rights Reserved") * pdf.internal.getFontSize(), pdf.internal.pageSize.height - 30);
-
+    pdf.text("2023 @Medicall | All Rights Reserved", pdf.internal.pageSize.width - 40 - pdf.getStringUnitWidth("2023 @MediCall | All Rights Reserved") * pdf.internal.getFontSize(), pdf.internal.pageSize.height - 30);
 
     pdf.save("Medicall-Invoice.pdf");
 
@@ -543,7 +572,6 @@ const MeetPage = () => {
                         <div className="item" key={index}>
                             <div className="prescription">
                                 <p>{item}</p>
-                                <div className="tooltip">{item}</div>
                             </div>
                             <div className="delete-item">
                                 <span onClick={() => {deletePrescriptionItem(index)}}>
@@ -556,19 +584,58 @@ const MeetPage = () => {
                 </div>
             )}
             <div className="new_prescription">
-                <div className="input_box">
-                    <input
-                        type="text"
-                        className="input_field"
-                        value={newPrescription}
-                        onChange={(e) => setNewPrescription(e.target.value)}
-                        placeholder="Add Something..."
-                        required
-                    />
+                <div className="input_boxes">
+                    <div className="input_box">
+                      <input
+                          type="text"
+                          className="input_field"
+                          value={newPrescription.name}
+                          onChange={(e) => setNewPrescription({...newPrescription, name: e.target.value})}
+                          placeholder="Medicine Name"
+                      />
+                    </div>
+                    <div className="input_box">
+                      <div className="box">
+                        <input
+                            type="text"
+                            className="input_field"
+                            value={newPrescription.dosage}
+                            onChange={(e) => {
+                              setInvDosage(!(/^[0-9]-[0-9]-[0-9]$/.test(e.target.value)));
+                              setNewPrescription({...newPrescription, dosage: e.target.value});
+                            }}
+                            placeholder="Dosage i.e. 1-0-0"
+                        />
+                        <select value={newPrescription.dosageTime} onChange={(e) => setNewPrescription({...newPrescription, dosageTime: e.target.value})}>
+                          <option value="Before Food">Before Food</option>
+                          <option value="After Food">After Food</option>
+                        </select>
+                      </div>
+                      <div className="box">
+                        <input
+                            type="text"
+                            className="input_field"
+                            value={newPrescription.duration}
+                            onChange={(e) => {
+                              setInvDuration(!(/^[0-9]{1,9}$/.test(e.target.value)));
+                              setNewPrescription({...newPrescription, duration: e.target.value});
+                            }}
+                            placeholder="Duration"
+                        />
+                        <select value={newPrescription.durationUnit} onChange={(e) => setNewPrescription({...newPrescription, durationUnit: e.target.value})}>
+                          <option value="day(s)">day(s)</option>
+                          <option value="month(s)">month(s)</option>
+                        </select>
+                      </div>
+                    </div>
                 </div>
-                <button onClick={addPrescriptionItem} disabled={newPrescription.length===0}>
-                    Add
-                </button>
+                <div className="add-btn">
+                  <button onClick={addPrescriptionItem} disabled={newPrescription.name.length===0 || isInvDosage || isInvDuration}>
+                      Add
+                  </button>
+                  {isInvDosage && <Alert severity="error">Dosage should be in the form of n-n-n</Alert>}
+                  {isInvDuration && <Alert severity="error">Invalid Duration</Alert>}
+                </div>
             </div>
             <div className="send-prescription">
                 <button className="send-btn" onClick={handleFormSubmit}>Send</button>
